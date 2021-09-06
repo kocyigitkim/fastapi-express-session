64657548674481da56e0acded0f75d923d64cfe0
Muhammed Koçyiğit <muhammetkocyigit0@gmail.com>
2021-09-06 15:51:16 +0300

    useragent and ip check added.

--- example.js
+++ example.js
@@ -9,7 +9,9 @@ app.use(new express_session.SessionManager().use);
 
 app.get("/", (req, res, next) => {
     res.json(req.session);
-    req.session.test = new Date();
+    if (req.sessionId === "81371c4d-2b57-4797-aab7-6636890e7991") {
+        req.session.test = new Date();
+    }
 });
 
 app.listen(5000, () => {
--- src/SessionManager.js
+++ src/SessionManager.js
@@ -12,25 +12,41 @@ class SessionManager {
     async use(req, res, next) {
         const _self = this;
         var sessionId = req.header("sessionid");
+        var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        var userAgent = req.headers['user-agent'];
+        var isNewSession = false;
+        var isGranted = true;
         if (sessionId) {
             try {
                 _self.store.touch(sessionId, req.session);
             } catch (err) { console.error(err); }
             var result = await waitCallback(_self.store, _self.store.get, sessionId);
-            req.session = result || {};
+            if (!result) {
+                isNewSession = true;
+                result = { session: {}, ip: ip, userAgent: userAgent };
+            }
+            req.session = result.session || {};
+            if (result && (result.ip != ip || result.userAgent != userAgent)) {
+                isGranted = false;
+                req.session = {};
+                req.sessionId = uuid();
+                req.userAgent = userAgent;
+                sessionId = req.sessionId;
+            }
         }
         else {
             sessionId = uuid();
             res.setHeader("sessionid", sessionId);
             var result = await waitCallback(_self.store, _self.store.set, sessionId, {});
             req.session = {};
+            isNewSession = true;
         }
         res.on('finish', () => {
             if (!req.session && req.sessionId) {
                 waitCallback(_self.store, _self.store.destroy, sessionId);
             }
             else {
-                waitCallback(_self.store, _self.store.set, sessionId, req.session);
+                waitCallback(_self.store, _self.store.set, sessionId, { session: req.session, ip: ip, userAgent: userAgent });
             }
         });
         req.sessionManager = _self;
